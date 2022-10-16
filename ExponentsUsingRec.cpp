75d2c9f4421e1c9aa23678395243cd6df67036be
hritik <chauhanghritik@gmail.com>
2022-10-16 17:18:23 +0530

    Exponent using Rec

--- ExponentsUsingRec.cpp
+++ ExponentsUsingRec.cpp
@@ -7,6 +7,14 @@ long long exp(int n, int power){
     else return n * exp(n,power/2) * exp(n,power/2); 
 }
 
+// an even more optimized approach would be- tho 
+/*long long exp(int n, int power){
+    if(power == 1) return n;
+    long long ans = exp(n,n/2); //only one callmade compared to 2 made above
+    if(power%2 == 0) return ans * ans;
+    else return n * ans * ans; 
+}*/
+
 int main() 
  { 
     int n, power;
